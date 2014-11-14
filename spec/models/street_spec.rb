8819527e8887bff37e0a951ea501b8f79fcaa35b
pezholio <pezholio@gmail.com>
2014-11-14 12:12:24 +0000

    Use latlng

--- spec/models/street_spec.rb
+++ spec/models/street_spec.rb
@@ -10,15 +10,15 @@ describe Street do
           settlement: "Saltburn-by-the-Sea",
           locality: "Saltburn, Marske and New Marske",
           authority: "Redcar and Cleveland (B)",
-          location: [466096, 521260]
+          location: [54.582231587, -0.97886718711]
         )
 
     expect(street.name).to eq("ZETLAND TERRACE")
     expect(street.settlement).to eq("Saltburn-by-the-Sea")
     expect(street.locality).to eq("Saltburn, Marske and New Marske")
     expect(street.authority).to eq("Redcar and Cleveland (B)")
-    expect(street.location.x).to eq(466096)
-    expect(street.location.y).to eq(521260)
+    expect(street.location.x).to eq(54.582231587)
+    expect(street.location.y).to eq(-0.97886718711)
   end
 
 end
