1aea6d80d6ef91fd3e9f847bbb4399b3fccdea84
pezholio <pezholio@gmail.com>
2014-11-21 12:45:45 +0000

    Namespace Mongoid::Geospatial::Point

--- app/models/concerns/address_part.rb
+++ app/models/concerns/address_part.rb
@@ -1,17 +1,17 @@
 module AddressPart
-  
+
   extend ActiveSupport::Concern
-  
+
   included do
-  
+
     include Tokenable
     include Mongoid::Geospatial
 
     validates_presence_of :name
 
     field :name, type: String
-    field :lat_lng, type: Point
-    field :easting_northing, type: Point
+    field :lat_lng, type: Mongoid::Geospatial::Point
+    field :easting_northing, type: Mongoid::Geospatial::Point
 
     index({ name: 1 })
     index({lat_lng: "2d"})
