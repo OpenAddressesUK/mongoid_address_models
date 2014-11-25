7face84528cde80349e86082d433bff4da2baf84
James Smith <james@floppy.org.uk>
2014-11-25 12:24:25 +0000

    proper rails engine initialisation
    including load path for models

--- lib/mongoid_address_models.rb
+++ lib/mongoid_address_models.rb
@@ -2,13 +2,7 @@ require 'mongoid'
 require 'mongoid_token'
 require 'mongoid_geospatial'
 
-begin
-  module MongoidAddressModels
-    class Engine < ::Rails::Engine
-      isolate_namespace MongoidAddressModels
-    end
-  end
-rescue NameError
-  module MongoidAddressModels
-  end
+module MongoidAddressModels
 end
+
+require 'mongoid_address_models/engine' if defined?(Rails)
---
+++ lib/mongoid_address_models/engine.rb
@@ -0,0 +1,9 @@
+module MongoidAddressModels
+  class Engine < ::Rails::Engine
+    
+    initializer 'mongoid_address_models.autoload', :before => :set_autoload_paths do |app|
+      model_path = File.expand_path("../../../app/models", __FILE__)
+      app.config.eager_load_paths << model_path    
+    end
+  end
+end
