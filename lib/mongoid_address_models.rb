7c705baf36b17ca47c0b485c809f7d9be2854c19
James Smith <james@floppy.org.uk>
2015-03-24 11:59:40 +0000

    fix lat-long confusion in tests
    also added aliases for x and y to long and lat in Mongoid::Point

--- lib/mongoid_address_models.rb
+++ lib/mongoid_address_models.rb
@@ -5,4 +5,5 @@ require 'mongoid_geospatial'
 module MongoidAddressModels
 end
 
+require 'mongoid_address_models/mongoid_point_patch'
 require 'mongoid_address_models/engine' if defined?(Rails)
--- lib/mongoid_address_models/factories/street_factory.rb
+++ lib/mongoid_address_models/factories/street_factory.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :street do
     name "Fake Street"
     provenance executed_at: "now", code: "some codez", nested: { this: "works" }
+    lat_lng [-2.242835, 57.101478]
   end
 
 end
