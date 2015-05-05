1c0f3dc2ca3f65a01ad4d0c2633e71da153cf1d0
pikesley <github@orgraphone.org>
2015-05-05 14:43:42 +0100

    Ordering is important

--- app/models/address.rb
+++ app/models/address.rb
@@ -10,8 +10,8 @@ class Address
   validates_presence_of :pao, if: :sao_blank?
   validates_presence_of :sao, if: :pao_blank?
 
-  field :pao, type: String
   field :uprn, type: String
+  field :pao, type: String
   field :sao, type: String
   field :full_address, type: String
   field :full_address_lines, type: Array
